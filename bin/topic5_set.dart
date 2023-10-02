a210f6905a3090a983a020835c5a2681c8a7043b
Erkeulan-Tolendi <tolendierkebulan555@gmail.com>
2023-10-02 12:04:23 +0600

    first commit

---
+++ bin/topic5_set.dart
@@ -0,0 +1,77 @@
+
+
+
+  void main() {
+  // Set<int> set1 = {1, 2, 3, 4, 5};
+  // Set<int> set2 = {2, 4};
+
+
+  // bool isSubset1 = set1.containsAll(set2);
+
+
+  // bool isSubset2 = set2.every((element) => set1.contains(element));
+
+  // print("Set 2 is a subset of Set 1 (Method 1): $isSubset1");
+  // print("Set 2 is a subset of Set 1 (Method 2): $isSubset2");
+
+  // Set<int> set = {1, 2, 3, 4, 5};
+  // set.clear();
+  // print(set);
+
+  // Set<int> set = {2,4,545,54,435,3,54,3,34,53,45,356,76,78,9,1032,878};
+  //  List<int> myList = set.toList();
+
+  // myList.sort();
+
+  //   int minValue = myList[0];
+
+  //   int maxValue = myList[myList.length - 1];
+
+  //   print("Set: $myList");
+  //   print("Minimum value: $minValue");
+  //   print("Maximum value: $maxValue");
+
+  // Set<int> set = {2,4,545,54,435,3,54,3,34,53,45,356,76,78,9,1032,878};
+  // List<int> myList = set.toList();
+  // var lengthset = myList.length;
+  // print(lengthset);
+
+
+  // Set<int> mySet = {1, 2, 3, 4, 5};
+  // int valueToCheck = 3;
+
+  // if (mySet.contains(valueToCheck)) {
+  //   print("$valueToCheck is present in the set.");
+  // } else {
+  //   print("$valueToCheck is not present in the set.");
+
+  // Set<int> mySet = {2,3,4,5,6,7,8,9};
+  // Set<int> mySet2 = {1,2,45,34,67,87,23};
+  // bool OnTest = true;
+
+  // for (int element in mySet){
+  //   if (mySet2.contains(element)){
+  //     OnTest = false;
+  //     break;
+  //   }
+  // }
+
+  // if (OnTest){
+  //   print("doesnt contain");
+  // }else{
+  //   print("contain");
+  // }
+  
+  // Set<int> mySet = {2,3,4,5,6,7,8,9};
+  // Set<int> mySet2 = {1,2,45,34,67,4};
+  // bool OnTest = true;
+
+  // for (int element in mySet){
+  //   if (mySet2.contains(element)){
+  //     print(element);
+  //   }
+  // }
+
+
+}
+
